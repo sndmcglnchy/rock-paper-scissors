55791103fe4e66f9b09e570b6c94d2a5a6412566
sndmcglnchy <sineadmcg.18@gmail.com>
2021-09-30 13:01:20 +0100

    Functionality added to game

--- script.js
+++ script.js
@@ -1,85 +1,135 @@
 
+
+
+// function checkUserSelection(){    
+//     //CHECKING IF BOTH ARE TRUE - IS THERE A VALUE IN USERINPUT
+//     //If so, hold the value entered by the user in the userSelection variable. Make this value the first parameter of playRound() and also call that function.
+//     if (userInput && userInput.value) {
+//                 let userSelection = userInput.value;
+//                 console.log(userSelection);
+//                 playRound(userSelection, computerPlay()); 
+
+//     }
+//     //otherwise, prompt the user to enter some text.
+//     else{
+//         result.textContent = "Please enter your weapon of choice above."
+//     }
+// }
+
 let result = document.querySelector('.result');
 let computer = document.querySelector('.computerChoice');
-let userInput = document.querySelector('.user-input');
-// let userSubmit = document.querySelector('.userSubmit');
+let scores = document.querySelector('.scores');
+let userScore = 0;
+let computerScore = 0;
+let resetButton;
+// let promptInput;
 
 function computerPlay(){
     // randomly return either Rock, Paper or Scissors
-    //use console.log() to make sure it's returning the expected output
     let options = ['Rock', 'Paper', 'Scissors'];
 
     // console.log(options[Math.floor(Math.random() * options.length)]);
     return options[Math.floor(Math.random() * options.length)];
     
-}
-
-function checkUserSelection(){    
-    //    let userInput = document.querySelector('.user-input');
-    //    let userSelection = userInput.value; 
-    //     console.log(userSelection);
-    //     playRound(userSelection, computerPlay());
-    //    return userSelection;
-    // let userInput = document.querySelector('.user-input');
-    //CHECKING IF BOTH ARE TRUE - IS THERE A VALUE IN USERINPUT
-    //If so, hold the value entered by the user in the userSelection variable. Make this value the first parameter of playRound() and also call that function.
-    if (userInput && userInput.value) {
-        let userSelection = userInput.value;
-        console.log(userSelection);
-        playRound(userSelection, computerPlay()); 
-    }
-    //otherwise, prompt the user to enter some text.
-    else{
-        result.textContent = "Please enter your weapon of choice above."
-    }
-}
+}s
 
 function playRound(playerSelection, computerSelection){  
     
-     if(playerSelection.toUpperCase() === 'ROCK' && computerSelection.toUpperCase() === 'ROCK'){
-        computer.textContent = "Rock VS. Rock";
+    if(playerSelection.toUpperCase() === 'ROCK' && computerSelection.toUpperCase() === 'ROCK'){
+        // computer.textContent = "Rock VS. Rock";
+
         result.textContent = "Oops! It's a tie";
+        console.log("Oops! It's a tie");
+
+        scores.textContent = "Your Score: " + userScore + ", Computer Score: " + computerScore;
+        console.log("Your Score: " + userScore + ", Computer Score: " + computerScore);
         return "Oops! It's a tie";
         
      }
      else if(playerSelection.toUpperCase() === 'ROCK' && computerSelection.toUpperCase() === 'PAPER'){
-        computer.textContent = "Rock VS. Paper";
+        computerScore += 1;
+        // computer.textContent = "Rock VS. Paper";
+
         result.textContent = "You lose! Paper beats Rock.";
+        console.log("You lose! Paper beats Rock.");
+
+        scores.textContent = "Your Score: " + userScore + ", Computer Score: " + computerScore;
+        console.log("Your Score: " + userScore + ", Computer Score: " + computerScore);
         return "You lose! Paper beats Rock.";
     }
     else if(playerSelection.toUpperCase() === 'ROCK' && computerSelection.toUpperCase() === 'SCISSORS'){
-        computer.textContent = "Rock VS. Scissors";
+        userScore += 1;
+        // computer.textContent = "Rock VS. Scissors";
+
         result.textContent = "You win! Rock defeats Scissors!";
+        console.log("You win! Rock defeats Scissors!");
+
+        scores.textContent = "Your Score: " + userScore + ", Computer Score: " + computerScore;
+        console.log("Your Score: " + userScore + ", Computer Score: " + computerScore);
         return "You win! Rock defeats Scissors!";
     }
     else if(playerSelection.toUpperCase() === 'PAPER' && computerSelection.toUpperCase() === 'PAPER'){
-        computer.textContent = "Paper VS. Paper";
+        // computer.textContent = "Paper VS. Paper";
+
         result.textContent = "Oops! It's a tie";
+        console.log("Oops! It's a tie");
+
+        scores.textContent = "Your Score: " + userScore + ", Computer Score: " + computerScore;
+        console.log("Your Score: " + userScore + ", Computer Score: " + computerScore);
         return "Oops! It's a tie";
     }
     else if(playerSelection.toUpperCase() === 'PAPER' && computerSelection.toUpperCase() === 'ROCK'){
-        computer.textContent = "Paper VS. Rock";
+        userScore += 1;
+        // computer.textContent = "Paper VS. Rock";
+
         result.textContent = "Congrats! You win - Paper beats Rock!";
+        console.log("Congrats! You win - Paper beats Rock!");
+
+        scores.textContent = "Your Score: " + userScore + ", Computer Score: " + computerScore;
+        console.log("Your Score: " + userScore + ", Computer Score: " + computerScore);
         return "Congrats! You win - Paper beats Rock!";
     }
     else if(playerSelection.toUpperCase() === 'PAPER' && computerSelection.toUpperCase() === 'SCISSORS'){
-        computer.textContent = "Paper VS. Scissors";
+        computerScore += 1;
+        // computer.textContent = "Paper VS. Scissors";
+
         result.textContent = "Oh no, you lose. Scissors beats Paper";
+        console.log("Oh no, you lose. Scissors beats Paper");
+
+        scores.textContent = "Your Score: " + userScore + ", Computer Score: " + computerScore;
+        console.log("Your Score: " + userScore + ", Computer Score: " + computerScore);
         return "Oh no, you lose. Scissors beats Paper";
     }
     else if(playerSelection.toUpperCase() === 'SCISSORS' && computerSelection.toUpperCase() === 'SCISSORS'){
-        computer.textContent = "Scissors VS. Scissors";
+        // computer.textContent = "Scissors VS. Scissors";
+
         result.textContent = "Oops! It's a tie";
+        console.log("Oops! It's a tie");
+
+        scores.textContent = "Your Score: " + userScore + ", Computer Score: " + computerScore;
+        console.log("Your Score: " + userScore + ", Computer Score: " + computerScore);
         return "Oops! It's a tie";
     }
     else if(playerSelection.toUpperCase() === 'SCISSORS' && computerSelection.toUpperCase() === 'ROCK'){
-        computer.textContent = "Scissors VS. Rock";
+        computerScore += 1;
+        // computer.textContent = "Scissors VS. Rock";
+
         result.textContent = "Aw you lose. Rock beats Scissors.";
+        console.log("Aw you lose. Rock beats Scissors.");
+
+        scores.textContent = "Your Score: " + userScore + ", Computer Score: " + computerScore;
+        console.log("Your Score: " + userScore + ", Computer Score: " + computerScore);
         return "Aw you lose. Rock beats Scissors.";
     }
     else if(playerSelection.toUpperCase() === 'SCISSORS' && computerSelection.toUpperCase() === 'PAPER'){
-        computer.textContent = "Scissors VS. Paper";
+        userScore += 1;
+        // computer.textContent = "Scissors VS. Paper";
+
         result.textContent = "Woohoo! Scissors beats Paper";
+        console.log("Woohoo! Scissors beats Paper");
+
+        scores.textContent = "Your Score: " + userScore + ", Computer Score: " + computerScore;
+        console.log("Your Score: " + userScore + ", Computer Score: " + computerScore);
         return "Woohoo! Scissors beats Paper";        
     }
     else{
@@ -88,4 +138,24 @@ function playRound(playerSelection, computerSelection){
     }
  }
 
-// playRound(checkUserSelection(), computerPlay());
+
+function game(){
+    for (i=0; i <= 4; i++){     
+        let promptInput = prompt("Rock, Paper or Scissors?");  
+        playRound(promptInput, computerPlay());        
+    }
+    if(userScore > computerScore){
+        console.log("Game Over! Congratulations! You won!");
+        result.textContent = "Game Over! Congratulations! You won!";
+    }
+    else if(computerScore > userScore){
+        console.log("Game Over! Bad luck. You lost.");
+        result.textContent = "Game Over! Bad luck. You lost.";
+    }
+    else{
+        console.log("Game Over! It's a tie! Try again?")
+        result.textContent = "Game Over! It's a tie! Try again?";
+    }
+
+}
+
